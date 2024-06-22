3c7bd6802f21a744758ab6b3115369dd050433ed
Tharindu Dananjaya <dananjayaabeytd@gmail.com>
2024-06-22 21:39:28 +0530

    called first API

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/api/products/product.service';
 
 @Component({
   selector: 'app-about',
@@ -9,7 +10,8 @@ import { ActivatedRoute } from '@angular/router';
 export class AboutComponent implements OnInit{
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private service:ProductService
   ){
     
   }
@@ -18,11 +20,22 @@ export class AboutComponent implements OnInit{
   queryParam:any;
 
   ngOnInit(): void {
+    
+
     console.log(this.activatedRoute)
     this.param = this.activatedRoute.snapshot.params['username'];
     this.queryParam = this.activatedRoute
       .snapshot
       .queryParams['course'];
+
+      //get products 
+      this.service.getAllProductsWithLimit()
+      .subscribe({
+        next:(data)=>{
+          console.log(data)
+        }
+      })
+
   }
 
 }
--- src/app/services/api/products/product.service.ts
+++ src/app/services/api/products/product.service.ts
@@ -1,17 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl: string = 'https://fakestoreapi.com/'
+  private baseUrl: string = 'https://fakestoreapi.com'
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
 
-  getAllProductsWithLimit(limit:number = 5):void{
+  getAllProductsWithLimit(limit:number = 5):Observable<any>{
     // const productsUrl2:string = this.baseUrl + '/products?limit=' + 5
-    const productsUrl:string = `${this.baseUrl} + '/products?limit=' + ${limit}`
+    const productsUrl:string = `${this.baseUrl}/products?limit=${limit}`;
+    return this.http.get<any>(productsUrl);
   }
 }
